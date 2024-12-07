e45774a35273a7ba6ef6f91310503cb58f6d1a53
Alexey Kolch <alexeykolychenkov94@gmail.com>
2024-12-07 17:38:56 +0300

    Initial Commit

---
+++ MyVideoPlayer/MyVideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyVideoPlayerApp.swift
+//  MyVideoPlayer
+//
+//  Created by Алексей Колыченков on 07.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct MyVideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
